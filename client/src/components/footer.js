d3011c8151300d45314ab23c8ff97aca54af337a
KerronKing <king_of_palmiste@hotmail.com>
2020-03-10 15:15:46 -0400

    style: footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => (
-  <div>
-    <Link to="/budgets/new">Add Budget (new)</Link>
+  <div className="footer">
+    <Link to="/budgets/new">Create New Budget</Link>
     <Link to="/budgets">Your Budgets</Link>
-    <Link to="/budgets/:budget_id/budget_totals">Stats</Link>
     <Link to="/user">Profile</Link>
+    <Link to="/logout">Logout</Link>
   </div>
 );
 
