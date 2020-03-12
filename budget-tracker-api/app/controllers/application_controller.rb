50d919cd73267b52bbbef690717303d5029382df
KerronKing <king_of_palmiste@hotmail.com>
2020-03-11 22:03:58 -0400

    refactor: move API to new repo and refactor front-end directory

--- budget-tracker-api/app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::API
-  include Knock::Authenticable
-  include Response
-  include ExceptionHandler
-  include ActionController::Serialization
-end
--- budget-tracker-api/app/controllers/user_token_controller.rb
+++
@@ -1,3 +0,0 @@
-class UserTokenController < Knock::AuthTokenController
-  skip_before_action :verify_authenticity_token, raise: false
-end
