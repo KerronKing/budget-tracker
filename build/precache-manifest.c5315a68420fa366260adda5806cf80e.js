0d75c15b307a985d2aea1295d1f8bd8f110a33d0
KerronKing <king_of_palmiste@hotmail.com>
2020-03-12 01:53:04 -0400

    chore: add gh pages package

---
+++ build/precache-manifest.c5315a68420fa366260adda5806cf80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0075200b9e4e735a2df4262ea0037b",
+    "url": "/budget-tracker/index.html"
+  },
+  {
+    "revision": "547c1c2ebf132e912118",
+    "url": "/budget-tracker/static/css/main.10eaa41a.chunk.css"
+  },
+  {
+    "revision": "173c4a4d4d323615d4ac",
+    "url": "/budget-tracker/static/js/2.92847ff8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/budget-tracker/static/js/2.92847ff8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "547c1c2ebf132e912118",
+    "url": "/budget-tracker/static/js/main.848182f9.chunk.js"
+  },
+  {
+    "revision": "20b4d24dfc60c3bc1f49",
+    "url": "/budget-tracker/static/js/runtime-main.b2884ff0.js"
+  }
+]);
