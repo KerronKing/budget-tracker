d8bd9a71eb8913064d7ce3eb3e39bb5844ffa235
KerronKing <king_of_palmiste@hotmail.com>
2020-06-14 11:44:55 -0400

    style: edit homepage styling

--- src/components/home-page.js
+++ src/components/home-page.js
@@ -20,8 +20,13 @@ const Homepage = ({ userStatus, name }) => {
       <div>
         <h1>Budget Tracker</h1>
         <div className="login-links">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign up</Link>
+          <div>
+            <img src="bug.png" height="250" width="250" alt="budget-img" />
+          </div>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign up</Link>
+          </div>
         </div>
       </div>
     );
